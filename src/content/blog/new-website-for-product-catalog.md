9d3b190ebec25480d76551ece9b819f39e2e633d
Seyed Sadegh Ataii <cigoler@gmail.com>
2023-10-26 09:33:20 +0100

    just testing

---
+++ src/content/blog/new-website-for-product-catalog.md
@@ -0,0 +1,17 @@
+---
+title: 'New Website'
+description: 'I have got a new website that will house my product catalog'
+category: 'News'
+tags: [
+    'Website', 
+    'News', 
+    'Products', 
+    ]
+featured: true
+pubDate: '2023-10-26'
+# heroImage: '/images/dexter_light.png'
+---
+
+# title for the website
+
+I've got a new website that will house my product catalog that will contain all of my 3D printable content that you can you access from my Patreon subscription.
--- src/content/config.ts
+++ src/content/config.ts
@@ -16,4 +16,19 @@ const products = defineCollection({
 	}),
 });
 
-export const collections = { products };
+const blog = defineCollection({
+	// Type-check frontmatter using a schema
+	schema: z.object({
+		title: z.string(),
+		description: z.string(),
+        category: z.string().default('uncategorized'),
+		tags: z.array(z.string()).default([]),
+		featured: z.boolean().default(false),
+		// Transform string to Date object
+		pubDate: z.coerce.date(),
+		updatedDate: z.coerce.date().optional(),
+		heroImage: z.string().optional(),
+	}),
+});
+
+export const collections = { products, blog };
