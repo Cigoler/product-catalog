7fc138e10ab857c8efde50e455f679764b05240a
Seyed Sadegh Ataii <cigoler@gmail.com>
2023-11-03 21:12:30 +0000

    accordion working

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -5,57 +5,66 @@ import {
     AccordionTrigger,
   } from "@/components/ui/accordion"
 
-// export default function Category() {
-//   return (
-//     <Accordion type="single" collapsible>
-//         <AccordionItem value="item-1">
-//             <AccordionTrigger>Is it accessible?</AccordionTrigger>
-//             <AccordionContent>
-//                 Yes. It adheres to the WAI-ARIA design pattern.
-//             </AccordionContent>
-//         </AccordionItem>
-//     </Accordion>
-//   )
-// }
 
-
-
-function AccordionItems({ value, children }) {
+/**
+ * Renders a wrapper component that displays a list of items in an accordion.
+ * @param {Object} props - The component props.
+ * @param {Array} props.items - The list of items to display.
+ * @param {string} [props.type="single"] - The type of accordion to display.
+ * @param {boolean} [props.collapsible=true] - Whether the accordion is collapsible or not.
+ * @returns {JSX.Element} - The rendered component.
+ */
+function AccordionWrapper({items, type="single", collapsible=true}) {
     return (
-        <div className="accordion-item">
-            <AccordionTriggers>{value}</AccordionTriggers>
-            <AccordionContents>{children}</AccordionContents>
-        </div>
-    );
+        <Accordion type={type} collapsible={collapsible}>
+            <Items items={items}/>
+        </Accordion>
+    )
 }
 
-function Accordions({ items }) {
+/**
+ * Renders a list of items as an accordion.
+ * @param {Object[]} items - An array of objects representing the items to be rendered.
+ * @param {string} items[].value - The value of the item.
+ * @param {string} items[].title - The title of the item.
+ * @param {string} items[].content - The content of the item.
+ * @returns {JSX.Element} - The rendered accordion items.
+ */
+function Items({items}) {
     return (
-        <div className="accordion">
+        <>
             {items.map((item, index) => (
-                <AccordionItems key={index} value={item.value}>
-                    {item.content}
-                </AccordionItems>
+                    <AccordionItem value={item.value} key={index}>
+                        <AccordionTrigger>
+                            {item.title}
+                        </AccordionTrigger>
+                        <AccordionContent>
+                            {item.content}
+                        </AccordionContent>
+                    </AccordionItem>
             ))}
-        </div>
-    );
+        </>
+    )
 }
 
-function AccordionWrapper({ children }) {
-    return <div className="accordion-wrapper">{children}</div>;
-}
+export { AccordionWrapper };
 
-function AccordionTriggers({ children, onClick }) {
-    return (
-        <div className="accordion-trigger" onClick={onClick}>
-            {children}
-        </div>
-    );
-}
+const sampleItems = [
+    {
+        value: "item1",
+        title: "Item 1",
+        content: "This is the content for item 1",
+    },
+    {
+        value: "item2",
+        title: "Item 2",
+        content: "This is the content for item 2",
+    },
+    {
+        value: "item3",
+        title: "Item 3",
+        content: "This is the content for item 3",
+    },
+];
 
-function AccordionContents({ children }) {
-    return <div className="accordion-content">{children}</div>;
-}
-
-export { Accordions, AccordionWrapper, AccordionTriggers, AccordionContents };
 
